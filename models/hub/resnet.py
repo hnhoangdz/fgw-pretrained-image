55ed5f5d8dfa0c4e3ac24815a2f2b920334c3344
hnhoangdz <snacky0907@gmail.com>
2024-05-07 10:09:54 +0700

    update models

--- models/hub/resnet.py
+++ models/hub/resnet.py
@@ -1,9 +1,10 @@
-from torchvision.models import resnet101, ResNet101_Weights
+from torchvision.models import resnet101, ResNet101_Weights, resnet50, ResNet50_Weights
 import torch
 import torch.nn as nn
+from torchvision.models.feature_extraction import create_feature_extractor, get_graph_node_names
 
-# def count_parameters(model): 
-#     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+from .fgw import Block, conv3x3
+from .mobilenetv2 import MobileNetV2
 
 class Model(nn.Module):
     def __init__(self, 
@@ -13,28 +14,97 @@ class Model(nn.Module):
         
         super(Model, self).__init__()
         
-        self.model = resnet101(weights=ResNet101_Weights.DEFAULT)
-        if in_channels != self.model.conv1.in_channels:
-            self.model.conv1 = nn.Conv2d(
+        resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
+        if in_channels != resnet.conv1.in_channels:
+            resnet.conv1 = nn.Conv2d(
                 in_channels, 64, 
                 kernel_size=(7, 7), 
                 stride=(2, 2), 
                 padding=(3, 3), 
                 bias=False
             )
-        # print(self.count_parameters())
+        ## freeze all layers
         if kwargs['freeze_layers'] == 'all':
-            for param in self.model.parameters():
+            for param in resnet.parameters():
                 param.requires_grad = False
-        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes, bias=True)
 
-    def count_parameters(self): 
-        return sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+        ## get feature extractor
+        self.extract_layer_name = kwargs['feature_extractor_name']
+        if self.extract_layer_name:
+            if kwargs['ft_name'] == 'linear':
+                return_nodes = {'avgpool': 'feature_extractor'}
+            else:
+                return_nodes = {self.extract_layer_name: 'feature_extractor'}
+            self.feature_extractor = create_feature_extractor(resnet, return_nodes=return_nodes)
+            last_layer = list(self.feature_extractor.modules())[-1]
+            # print(self.count_parameters(self.feature_extractor))
+            if isinstance(last_layer, nn.Conv2d):
+                self.last_out_channels = last_layer.out_channels # 256
+            elif isinstance(last_layer, nn.AdaptiveAvgPool2d):
+                self.last_out_channels = 2048
+            
+        if kwargs['ft_name'] == 'fgw':
+            self.model = nn.Sequential(
+                # fgw module
+                Block(self.last_out_channels, self.last_out_channels*2, keep_dim=False), # 256x28x28
+                Block(self.last_out_channels*2, self.last_out_channels*4, keep_dim=False), # 512x14x14
+                Block(self.last_out_channels*4, self.last_out_channels*4, keep_dim=False), # 512x7x7
+
+                # glb module
+                conv3x3(512, num_classes),
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Flatten()
+                
+                # # fcn module
+                # nn.Flatten(),
+                # nn.Linear(in_features=512*7*7, out_features=512),
+                # nn.ReLU(inplace=True),
+                # nn.Dropout(0.5),
+                # nn.Linear(512, num_classes, bias=True)
+            )
+        elif kwargs['ft_name'] == 'mobilenetv2':
+            self.model = MobileNetV2(self.last_out_channels,
+                                     num_classes=num_classes)
+        elif kwargs['ft_name'] == 'linear':
+            self.model = nn.Sequential(
+                nn.Sequential(
+                    nn.Flatten(),
+                    nn.Linear(self.last_out_channels, 1024),
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(0.4),
+                    
+                    nn.Linear(1024, 512),
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(0.4),
+                    
+                    nn.Linear(512, num_classes, bias=True)
+                )
+            )
+        total_feature_extractor_params, trainable_feature_extractor_params = self.count_parameters(self.feature_extractor)
+        # print(f'feature extractor params: {trainable_feature_extractor_params}/{total_feature_extractor_params}')
+        total_model_params, trainable_model_params = self.count_parameters(self.model)
+        # print(f'model params: {trainable_model_params}/{total_model_params}')
+        print('total parameters: ', total_feature_extractor_params + total_model_params)
+        print('trainable parameters: ', trainable_feature_extractor_params + trainable_model_params)
+        
+    def count_parameters(self, model): 
+        total_params = 0
+        trainable_params = 0
+        for p in model.parameters():
+            if p.requires_grad:
+                trainable_params += p.numel()
+            total_params += p.numel()
+        return total_params, trainable_params
+        # return sum(p.numel() for p in model.parameters() if p.requires_grad)
     
     def forward(self, x):
-        out = self.model(x)
+        with torch.no_grad():
+            out = self.feature_extractor(x)['feature_extractor']
+        print(out.shape)
+        out = self.model(out)
         return out
+    
 if __name__ == '__main__':
     x = torch.randn(1, 3, 128, 128)
-    model = Model(3, freeze_layers='all')
+    model = Model(3, 7, freeze_layers='linear')
     print(model(x).shape)
--- models/hub/vit.py
+++ models/hub/vit.py
@@ -1,6 +1,8 @@
-from torchvision.models import vit_l_32, ViT_L_32_Weights
+from torchvision.models import vit_l_32, ViT_L_32_Weights, vit_l_16, ViT_L_16_Weights
 import torch
 import torch.nn as nn
+from torchvision.models.feature_extraction import create_feature_extractor, get_graph_node_names
+
 
 class Model(nn.Module):
     def __init__(self, 
@@ -9,27 +11,62 @@ class Model(nn.Module):
                  *args, **kwargs):
         
         super(Model, self).__init__()
-        self.model = vit_l_32(weights=ViT_L_32_Weights.DEFAULT)
-        if in_channels != self.model.conv_proj.in_channels:
-            self.model.conv_proj = nn.Conv2d(
+        vit = vit_l_16(weights=ViT_L_16_Weights.DEFAULT)
+        if in_channels != vit.conv_proj.in_channels:
+            vit.conv_proj = nn.Conv2d(
                 in_channels, 1024, 
                 kernel_size=(32, 32), 
                 stride=(32, 32)
             )
         if kwargs['freeze_layers'] == 'all':
-            for param in self.model.parameters():
+            for param in vit.parameters():
                 param.requires_grad = False
-        self.model.heads.head = nn.Linear(
-            in_features=1024,
-            out_features=num_classes,
-            bias=True
-        )
+        self.extract_layer_name = kwargs['feature_extractor_name']
+        if self.extract_layer_name:
+            if kwargs['ft_name'] == 'linear':
+                return_nodes = {'encoder.ln': 'feature_extractor'}
+            else:
+                pass
+            self.feature_extractor = create_feature_extractor(vit, return_nodes=return_nodes)
+            self.last_out_channels = 201728
+        
+        if kwargs['ft_name'] == 'linear':
+            self.model = nn.Sequential(
+                nn.Sequential(
+                    nn.Flatten(),
+                    nn.Linear(self.last_out_channels, 1024),
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(0.4),
+                    
+                    nn.Linear(1024, 512),
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(0.4),
+                    
+                    nn.Linear(512, num_classes, bias=True)
+                )
+            )
 
-    def count_parameters(self): 
-        return sum(p.numel() for p in self.model.parameters() if p.requires_grad)
-    
+        total_feature_extractor_params, trainable_feature_extractor_params = self.count_parameters(self.feature_extractor)
+        # print(f'feature extractor params: {trainable_feature_extractor_params}/{total_feature_extractor_params}')
+        total_model_params, trainable_model_params = self.count_parameters(self.model)
+        # print(f'model params: {trainable_model_params}/{total_model_params}')
+        print('total parameters: ', total_feature_extractor_params + total_model_params)
+        print('trainable parameters: ', trainable_feature_extractor_params + trainable_model_params)
+        
+    def count_parameters(self, model): 
+        total_params = 0
+        trainable_params = 0
+        for p in model.parameters():
+            if p.requires_grad:
+                trainable_params += p.numel()
+            total_params += p.numel()
+        return total_params, trainable_params
+        
     def forward(self, x):
-        return self.model(x)
+        with torch.no_grad():
+            out = self.feature_extractor(x)['feature_extractor']
+        out = self.model(out)
+        return out
 
 if __name__ == '__main__':
     x = torch.randn(1, 3, 224, 224)
